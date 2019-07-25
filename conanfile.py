cf04951d38032e2450ddcbefc20e596826626e31
Michele Adduci <adduci.michele@gmail.com>
2019-07-25 21:31:08 +0200

    Added gsoap definition

--- conanfile.py
+++ conanfile.py
@@ -2,61 +2,93 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
+class GsoapConan(ConanFile):
+    name = "gsoap"
+    version_major = "2.8"
+    version = version_major + ".87"
+    description = "The gSOAP toolkit is a C and C++ software development toolkit for SOAP and " \
+                  "REST XML Web services and generic C/C++ XML data bindings."
     # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
+    topics = ("conan", "gsoap", "logging")
+    url = "https://github.com/bincrafters/conan-gsoap"
+    homepage = "https://www.genivia.com/"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    license = "https://www.genivia.com/products.html#gsoap"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
     # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = ["FindGSOAP.cmake", "src/*.cmake", "src/*.txt"]
     generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
+    short_paths = True
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
+    options = {"with_openssl": [True, False],
+               "with_ipv6": [True, False],
+               "with_cookies": [True, False],
+               "with_c_locale": [True, False]}
+    default_options = "with_openssl=True", "with_ipv6=True", \
+                      "with_cookies=True", "with_c_locale=True"
     # Custom attributes for Bincrafters recipe conventions
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
-    requires = (
-        "OpenSSL/1.0.2s@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+    def requirements(self):
+        if self.settings.os == "Windows":
+            self.requires("winflexbison/2.5.15@bincrafters/stable")
+        else:
+            self.requires("bison/3.3.2@bincrafters/stable")
+            self.requires("flex/2.6.4@bincrafters/stable")
+        if self.options.with_openssl:
+            self.requires("OpenSSL/1.1.1c@conan/stable")
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
-        extracted_dir = self.name + "-" + self.version
-
+        try:
+            tools.get("https://sourceforge.net/projects/gsoap2/files/{name}-{version_major}/{name}_{version}.zip/download".format(name=self.name, version_major=self.version_major, version=self.version))
+        except NotFoundException:  # Maybe it has been moved to `oldreleases`
+            tools.get("https://sourceforge.net/projects/gsoap2/files/oldreleases/{name}_{version}.zip/download".format(name=self.name, version=self.version))
         # Rename to "source_subfolder" is a convention to simplify later steps
+        extracted_dir = self.name + "-" + self.version_major
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
+        cmake.definitions["GSOAP_PATH"] = self._source_subfolder
+        
+        if self.options.with_openssl:
+            cmake.definitions["WITH_OPENSSL"] = True
+            cmake.definitions["WITH_GZIP"] = True
+        if self.options.with_ipv6:
+            cmake.definitions["WITH_IPV6"] = True
+        cmake.definitions["WITH_COOKIES"] = self.options.with_cookies
+        cmake.definitions["WITH_C_LOCALE"] = self.options.with_c_locale
+
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        if self.settings.os == "Windows":
+            cmake = self._configure_cmake()
+            cmake.build()
+            cmake.install()
+        else:
+            with chdir(self._source_subfolder):
+                self.run('chmod +x configure')
+                self.run('./configure --help')
+                env_build = AutoToolsBuildEnvironment(self)
+                self.run('autoreconf -f -i')  # Fix out of date aclocal
+                env_build.configure(args=['--prefix', self.package_folder,
+                                          '--with-openssl={}'.format(self.deps_cpp_info["OpenSSL"].rootpath),
+                                          '--with-zlib={}'.format(self.deps_cpp_info["zlib"].rootpath),
+                                          '--enable-ipv6' if self.options.with_ipv6 else '--disable-ipv6',
+                                          #'--enable-cookies' if self.options.with_cookies else '--disable-cookies',
+                                          '--enable-c-locale' if self.options.with_c_locale else '--disable-c-locale',
+                                          '--enable-debug' if self.settings.build_type == 'Debug' else '',
+                                          ],
+                                    build=False)
+                env_build.make(args=["-j1", ],)  # Weird, but with -j2 it fails
+                env_build.make(args=['install', ])
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
+        self.copy("FindGSOAP.cmake", dst=".", src=".")
         # If the CMakeLists.txt has a proper install method, the steps below may be redundant
         # If so, you can just remove the lines below
         include_folder = os.path.join(self._source_subfolder, "include")
@@ -68,4 +100,20 @@ class LibnameConan(ConanFile):
         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        defines = []
+        if self.options.with_openssl:
+            libs = ["gsoapssl++", ]
+            defines.append("WITH_OPENSSL")
+            defines.append("WITH_GZIP")
+        else:
+            libs = ["gsoap++", ]
+        self.cpp_info.libs = libs
+
+        if self.options.with_ipv6:
+            defines.append("WITH_IPV6")
+        if self.options.with_cookies:
+            defines.append("WITH_COOKIES")
+        if self.options.with_c_locale:
+            defines.append("WITH_C_LOCALE")
+        self.cpp_info.defines = defines
