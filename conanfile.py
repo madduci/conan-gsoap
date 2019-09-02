3e2e70cbe848a1d408f4c36acfe1960846e6629f
Uilian Ries <uilianries@gmail.com>
2019-09-02 19:28:19 -0300

    Fix licenses
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,13 +9,12 @@ class GsoapConan(ConanFile):
     version = version_major + ".91"
     description = "The gSOAP toolkit is a C and C++ software development toolkit for SOAP and " \
                   "REST XML Web services and generic C/C++ XML data bindings."
-    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
     topics = ("conan", "gsoap", "logging")
     url = "https://github.com/bincrafters/conan-gsoap"
-    homepage = "https://www.genivia.com/"
+    homepage = "https://www.genivia.com"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "gSOAP-1.3b"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports = ["LICENSE.md", "FindGSOAP.cmake"]      # Packages the license for the conanfile.py
+    license = "gSOAP-1.3b"
+    exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "src/*.cmake", "src/*.txt"]
     generators = "cmake"
     short_paths = True
@@ -24,10 +23,19 @@ class GsoapConan(ConanFile):
                "with_ipv6": [True, False],
                "with_cookies": [True, False],
                "with_c_locale": [True, False]}
-    default_options = {'with_openssl': True, 'with_ipv6': True, 'with_cookies': True, 'with_c_locale': True}
-    # Custom attributes for Bincrafters recipe conventions
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    default_options = {
+        'with_openssl': True,
+        'with_ipv6': True,
+        'with_cookies': True,
+        'with_c_locale': True}
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def build_requirements(self):
         if self.settings.os == "Windows":
@@ -53,35 +61,22 @@ class GsoapConan(ConanFile):
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["GSOAP_PATH"] = self._source_subfolder
-        
-        if self.options.with_openssl:
-            cmake.definitions["WITH_OPENSSL"] = True
-            cmake.definitions["WITH_GZIP"] = True
-        if self.options.with_ipv6:
-            cmake.definitions["WITH_IPV6"] = True
+        cmake.definitions["WITH_OPENSSL"] = self.options.with_openssl
+        cmake.definitions["WITH_IPV6"] = self.options.with_ipv6
         cmake.definitions["WITH_COOKIES"] = self.options.with_cookies
         cmake.definitions["WITH_C_LOCALE"] = self.options.with_c_locale
-
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("FindGSOAP.cmake", dst=".", src=".")
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="GPLv2_license.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
