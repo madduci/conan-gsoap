f5840301891bc450eeea38e855b656904ec7d035
bincrafters-user <bincrafters@gmail.com>
2020-04-09 06:13:17 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.23.4

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class ConanFileDefault(ConanFileBase):
 
     def requirements(self):
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1e")
+            self.requires("openssl/1.1.1f")
 
     def _configure_cmake(self):
         cmake = CMake(self)
